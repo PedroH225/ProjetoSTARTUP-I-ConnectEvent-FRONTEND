d859ba5a8b3bbef562123ffac35d8b4fa42f87c1
PedroH225 <pedrodguimaraes20@gmail.com>
2024-10-24 21:28:02 -0300

    Criacao das services cidades

---
+++ src/services/cidades.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CidadesService {
+  private apiUrl = 'http://localhost:3000'; // Endereço do backend
+
+  constructor(private http: HttpClient) {}
+
+  getTipoEvento(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/cidades`);
+  }
+}
+
