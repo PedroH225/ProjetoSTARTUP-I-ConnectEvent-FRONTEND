a95c9e98e9da691cf83f6d08aa95d20d5ddc6e12
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2024-10-29 21:57:35 -0300

    Implementar formulário usuario

--- src/app/principal/register/register.component.ts
+++ src/app/principal/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CidadesService } from '../../../services/cidades.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  selectedOption: string = 'usuario';
+    selectedOption: string = 'usuario';
+    selectedCidade : string = "";
+    cidades: any[] = []
+    nome : string = ''
+    email : string = ''
+    senha : string = ''
+    confirmacao : string = ''
+    genero : string = '';
+    estado : string = '';
+    idade !: number;
+  
+    constructor( private cidadesService : CidadesService, 
+    ) {}
+
+    ngOnInit(): void {
+      this.getCidades();
+    }
+
+    onSubmit() {
+      console.log("aaaa" + this.nome, this.email, this.senha, this.confirmacao, this.genero, this.estado, this.idade);
+      
+    }
+    
+  getCidades(): void {
+    this.cidadesService.getCidades().subscribe(
+      (response) => {
+        this.cidades = response;
+      },
+      (error) => {
+        console.error('Erro ao buscar eventos:', error);
+      }
+    );
+  }
+
 }
