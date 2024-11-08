632654a6cf0e0d10ffc23ca926d8b9564187cdd0
PedroH225 <pedrodguimaraes20@gmail.com>
2024-11-08 12:19:59 -0300

    Form estatisticas

--- src/app/principal/area-usuario/estatisticas/estatisticas.component.ts
+++ src/app/principal/area-usuario/estatisticas/estatisticas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventosService } from '../../../../services/eventos.service';
 
 @Component({
   selector: 'app-estatisticas',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class EstatisticasComponent {
 
+  usuarioEventos : any[] = []
+
+  selectedEvento: string | null = null;
+  selectedDados: string | null = null;
+
+  constructor(
+    private eventoService : EventosService
+  ) {}
+
+  ngOnInit() {
+    this.eventoService.getEventosUsuario().subscribe(
+      (eventos) => {
+        this.usuarioEventos = eventos;
+
+      },
+      (error) => {
+        console.log(error);
+        
+      }
+    )
+  }
+
+  onSubmit() {
+    console.log(this.selectedEvento, this.selectedDados);
+    
+  }
 }
