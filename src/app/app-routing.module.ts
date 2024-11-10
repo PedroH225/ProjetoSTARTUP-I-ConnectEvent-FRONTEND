9e5c71876cb9b18e9a451e247153393e6876ca6e
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-11-10 20:08:26 -0300

    ajuste routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
         canActivate: [authGuard],
         children: [
           // inicio do 'path que auxilia o desenvolvimento
-          { path: '', redirectTo: 'listaDeAmigos', pathMatch: 'full' }, // Redireciona para criarEvento ao carregar areaUsuario
+          // { path: '', redirectTo: 'listaDeAmigos', pathMatch: 'full' }, // Redireciona para criarEvento ao carregar areaUsuario
           // fim do 'path' que auxilia o desenvolvimento
           { path: 'configuracoes', component: ConfiguracoesComponent },
           { path: 'criarEvento', component: CriarEventoComponent },
