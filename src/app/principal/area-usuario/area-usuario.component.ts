7c3d9c72cdb7ab1025bc4c28b81e654d060f3d90
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-11-11 17:19:11 -0300

    ajuste area do usuario e criar evento

--- src/app/principal/area-usuario/area-usuario.component.ts
+++ src/app/principal/area-usuario/area-usuario.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
 import { AutenticacaoService } from '../../../services/autenticacao.service';
 import { Router } from '@angular/router';
+import {
+  NgbAccordionConfig,
+  NgbAccordionModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-area-usuario',
   templateUrl: './area-usuario.component.html',
-  styleUrl: './area-usuario.component.scss'
+  styleUrl: './area-usuario.component.scss',
 })
 export class AreaUsuarioComponent {
-  
   constructor(
-    private autenticacaoService : AutenticacaoService,
-    private router : Router
-  ) {}
-
-  logout() {
-    if (confirm("Tem certeza que deseja sair?")) {
-      this.autenticacaoService.logout();
-    this.router.navigate(["/principal/homePage"])
-    }
+    private autenticacaoService: AutenticacaoService,
+    private router: Router,
+    config: NgbAccordionConfig
+  ) {
+    config.closeOthers = true;
   }
 
+  logout() {
+    if (confirm('Tem certeza que deseja sair?')) {
+      this.autenticacaoService.logout();
+      this.router.navigate(['/principal/homePage']);
+    }
+  }
 }
