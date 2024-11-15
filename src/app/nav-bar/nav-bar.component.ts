1a0fcda6fa2ce91c9576cc088d247634c4a8e5fe
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-11-15 12:29:24 -0300

    tentativa de implementacao thema

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AutenticacaoService } from '../../services/autenticacao.service';
 import { Router } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent implements OnInit{
-  public autenticacao$ = this.autenticacaoService.autenticacao$; 
-    isMenuCollapsed = true;
-
+export class NavBarComponent implements OnInit {
+  public autenticacao$ = this.autenticacaoService.autenticacao$;
+  isMenuCollapsed = true;
 
   constructor(
-    private autenticacaoService : AutenticacaoService,
-    private router: Router
-    )  {}
+    private autenticacaoService: AutenticacaoService,
+    private router: Router,
+    private themeService: ThemeService
+  ) {}
 
-  ngOnInit(): void {    
-  }  
+  ngOnInit(): void {}
 
   logout() {
-    if (confirm("Tem certeza que deseja sair?")) {
-    this.autenticacaoService.logout();
-    this.router.navigate(["/principal/homePage"])
+    if (confirm('Tem certeza que deseja sair?')) {
+      this.autenticacaoService.logout();
+      this.router.navigate(['/principal/homePage']);
     }
   }
-}
 
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+}
