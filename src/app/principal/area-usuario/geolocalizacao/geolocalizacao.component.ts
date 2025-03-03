d60ca63f580c9cd5be13283ded8d15cd825e22d8
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2025-03-03 15:39:38 -0300

    .

--- src/app/principal/area-usuario/geolocalizacao/geolocalizacao.component.ts
+++ src/app/principal/area-usuario/geolocalizacao/geolocalizacao.component.ts
@@ -8,40 +8,41 @@ import { environment } from '../../../../../src/environments/environment';
   templateUrl: './geolocalizacao.component.html',
   styleUrl: './geolocalizacao.component.scss',
 })
-export class GeolocalizacaoComponent implements OnInit {
-  map: google.maps.Map | undefined; // Tipagem adequada para o mapa
+// export class GeolocalizacaoComponent implements OnInit {
+  export class GeolocalizacaoComponent {
+  // map: google.maps.Map | undefined; // Tipagem adequada para o mapa
 
-  constructor(private renderer: Renderer2) {}
+  // constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
-    this.loadGoogleMapsScript().then(() => {
-      this.initMap(); // Inicializa o mapa após carregar o script
-    });
-  }
+  // ngOnInit(): void {
+  //   this.loadGoogleMapsScript().then(() => {
+  //     this.initMap(); // Inicializa o mapa após carregar o script
+  //   });
+  // }
 
-  private initMap(): void {
-    const mapOptions: google.maps.MapOptions = {
-      center: { lat: -34, lng: 150 },
-      zoom: 8,
-    };
-    this.map = new google.maps.Map(
-      document.getElementById('map') as HTMLElement,
-      mapOptions
-    );
-  }
+  // private initMap(): void {
+  //   const mapOptions: google.maps.MapOptions = {
+  //     center: { lat: -34, lng: 150 },
+  //     zoom: 8,
+  //   };
+  //   this.map = new google.maps.Map(
+  //     document.getElementById('map') as HTMLElement,
+  //     mapOptions
+  //   );
+  // }
 
-  private loadGoogleMapsScript(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      if (document.getElementById('google-maps-script')) {
-        resolve(); // Já está carregado
-        return;
-      }
-      const script = this.renderer.createElement('script');
-      script.id = 'google-maps-script';
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&libraries=places`;
-      script.onload = () => resolve();
-      script.onerror = () => reject();
-      this.renderer.appendChild(document.body, script);
-    });
-  }
+  // private loadGoogleMapsScript(): Promise<void> {
+  //   return new Promise((resolve, reject) => {
+  //     if (document.getElementById('google-maps-script')) {
+  //       resolve(); // Já está carregado
+  //       return;
+  //     }
+  //     const script = this.renderer.createElement('script');
+  //     script.id = 'google-maps-script';
+  //     script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&libraries=places`;
+  //     script.onload = () => resolve();
+  //     script.onerror = () => reject();
+  //     this.renderer.appendChild(document.body, script);
+  //   });
+  // }
 }
